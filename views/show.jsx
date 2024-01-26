062dd94dafefb2237c452a761e3523700f9a94b4
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-01-25 19:20:48 -0500

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -18,9 +18,10 @@ function Show ({bread, index}) {
   <img src={bread.image} alt={bread.name} />
   <li><a href="/breads">Go home</a></li>
 
-<form action={`/breads/${index}?_method=DELETE`} method="POST">
+  <form action={`/breads/${index}?_method=DELETE`} method="POST">
   <input type='submit' value="DELETE"/>
-</form>
+  </form>
+  <a href={`/breads/${index}/edit`}><button>Edit</button></a>
 </Default>
 
     )
