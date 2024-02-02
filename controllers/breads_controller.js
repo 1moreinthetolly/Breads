c9881cd56ceed291b1c576ecff458dbefbe50c68
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-02-01 20:24:52 -0500

    Custom Helper Methods completed

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -32,14 +32,13 @@ breads.get('/:id/edit', (req, res) => {
 breads.get('/:id', (req, res) => {
      Bread.findById(req.params.id)
         .then(foundBread => {
-            res.render('show', {
-                bread: foundBread
-            })
+          const bakedBy = foundBread.getBakedBy()
+          console.log(bakedBy)
+          res.render('show', {
+              bread: foundBread
+          })
+        })  
     })
-    .catch(err => {
-      res.send('404')
-    })
-  })
   
 
 
