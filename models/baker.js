fa58c6bbcb4aa40e9d2f6f5a1998d96ab5ed02af
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-02-04 13:19:01 -0500

    BreadCRUD Virtuals complete

--- models/baker.js
+++ models/baker.js
@@ -1,6 +1,8 @@
 // dependencies
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const Bread = require('./bread')
+
 
 // schema
 const bakerSchema = new Schema({
@@ -13,7 +15,15 @@ const bakerSchema = new Schema({
         type: Date,
         required: true
     },
-    bio: String
+    bio: String,
+  }, { toJSON: { virtuals: true }})
+
+
+// Virtuals:
+bakerSchema.virtual('breads', {
+  ref: 'Bread',
+  localField: '_id',
+  foreignField: 'baker'
 })
 
 // model and export
