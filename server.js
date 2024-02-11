8bfb77dbd029a1db2512483a1bdba2cb07cd4996
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-02-11 12:41:38 -0500

    breadcrud database up to date

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT
 const app = express()
 //mongoose.connect(process.env.MONGO_URI)
 //console.log(`connected to mongo:`, process.env.MONGO_URI)
-mongoose.connect(process.env.MONGO_URI,).then(() => console.log('connected to mongo: ', process.env.MONGO_URI))
+mongoose.connect(process.env.MONGO_URI)
+console.log('connected to mongo: ', process.env.MONGO_URI)
 //mongoose.createConnection(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true}).asPromise(console.log('connected to mongo: ', process.env.MONGO_URI))
 
 
